875d4ef85a859763c9d121ce050ea42ca008d358
ElionEriksson <64314315+ElionEriksson@users.noreply.github.com>
2021-02-22 17:28:59 +0100

    removed bloat code

--- macd.py
+++ macd.py
@@ -1,13 +1,5 @@
 import yfinance as yf
 
-
-
-# tickers = ["all_tickers"]
-
-
-# data = yf.Ticker("AAPL")
-# print(data.info["fiftyDayAverage"])
-
 class StockData:
     def __init__(self, stock_ticker):
         self.stock_data = yf.Ticker(stock_ticker)
