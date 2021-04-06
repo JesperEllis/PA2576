23956ce677bb26fa3bd546c26a1500b2e06faa04
ElionEriksson <64314315+ElionEriksson@users.noreply.github.com>
2021-04-06 22:09:46 +0200

    Update method comments and errormanagement

--- mailsender_test.py
+++ mailsender_test.py
@@ -7,29 +7,48 @@ import ssl
 
 class MailSender:
     def __init__(self):
+        """attributes needed for SMTP server"""
         self._port = 465
         self._sender = "stockfluentinfo@gmail.com"
         self._password = "pa2576student"
         self._context = ssl.create_default_context()
 
     def reset_password(self, receiver):
+        """
+        Entry method for flask script to request reset password functionality
+        Currently the method does not know about tha DatabaseInterface and
+        therfore needs receiver email as parameter!!
+        
+        """
         #skicka med länk
         message = "Subject: Reset Password" + '\n\n' + f'{receiver}'
-        self._send_email(receiver, message=message)
+        try:
+            self._send_email(receiver, message=message)
+        except Exception:
+            return "Email does not exsist"
+        return "successfull"
 
     def send_recommendation(self, receiver):
+        """
+        Entry method for flask script to request reset password functionality
+        Currently the method does not know about tha DatabaseInterface and
+        therfore needs receiver email as parameter!!
+        
+        """
         message = "Subject: New Recommendation" + '\n\n' + f'{receiver}'
         self._send_email(receiver, message=message)
+
+        
     
     def _send_email(self, receiver, message):
         with smtplib.SMTP_SSL("smtp.gmail.com", self._port, context=self._context) as server:
             try:
                 server.login(self._sender, self._password)
-            except:
-                raise "Det gick inte att logga in"
-            server.sendmail(self._sender, receiver, message)
-            print("Successfully sent mail!")
+                server.sendmail(self._sender, receiver, message)
+                print("Successfully sent mail!")
+            except Exception:
+                return "Falut"
 
-
-ms = MailSender()
-ms.send_email("elion.eriksson@hotmail.com")
+if __name__ == '__main__':
+    ms = MailSender()
+    ms.send_email("elion.eriksson@hotmail.com")
