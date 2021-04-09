ca189697445d87b0c992f393e1d34bbc3276dc87
ElionEriksson <64314315+ElionEriksson@users.noreply.github.com>
2021-04-09 14:26:23 +0200

    Created Private code generator

--- mainframe/mailsender.py
+++ mainframe/mailsender.py
@@ -1,17 +1,16 @@
 import smtplib
 import ssl
-
-# user_emails = ["elion.eriksson@hotmail.com"]
-
-
+import random
+import string
 
 class MailSender:
-    def __init__(self):
+    def __init__(self, code_generator):
         """attributes needed for SMTP server"""
         self._port = 465
         self._sender = "stockfluentinfo@gmail.com"
         self._password = "pa2576student"
         self._context = ssl.create_default_context()
+        self._code_generator = code_generator
 
     def reset_password(self, receiver):
         """
@@ -20,13 +19,15 @@ class MailSender:
         therfore needs receiver email as parameter!!
         
         """
+        code = self._code_generator.generate_code()
         #skicka med länk
-        message = "Subject: Reset Password" + '\n\n' + f'{receiver}'
+        message = "Subject: Reset Password" + '\n\n' + f'A request to reset password on your account has been made! \n\nEnter the following code to choose a new password.\nYour code: {code}'
+        
         try:
             self._send_email(receiver, message=message)
         except Exception:
             return "Email does not exsist"
-        return "successfull"
+        return code
 
     def send_recommendation(self, receiver):
         """
@@ -38,8 +39,6 @@ class MailSender:
         message = "Subject: New Recommendation" + '\n\n' + f'{receiver}'
         self._send_email(receiver, message=message)
 
-        
-    
     def _send_email(self, receiver, message):
         with smtplib.SMTP_SSL("smtp.gmail.com", self._port, context=self._context) as server:
             try:
@@ -49,5 +48,10 @@ class MailSender:
             except Exception:
                 return "Falut"
 
+class CodeGenerator:
+    def generate_code(self):
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
 if __name__ == '__main__':
-    ms = MailSender()
+    ms = MailSender(CodeGenerator())
+    ms.reset_password('elion.eriksson@hotmail.com')
